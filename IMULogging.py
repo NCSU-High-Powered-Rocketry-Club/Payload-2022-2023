9fa2b887899bec764d49ef028f94ea31c043b280
Trent <tdcouse@ncsu.edu>
2022-11-14 21:15:44 +0000

    Add IMU test program stuff

--- IMULogging.py
+++ IMULogging.py
@@ -1,5 +1,18 @@
 import BNOInterface
-import adafruit_bno055
+import time
+
+bno = BNOInterface.BNOInterface()
+
 
 while True:
-    adafruit_bno055.
+    grav = bno.get_gravity()
+    grav_x = grav[0]
+    grav_y = grav[1]
+    grav_z = grav[2]
+
+    print(f'Gravity x : {grav_x}')
+    print(f'Gravity y : {grav_y}')
+    print(f'Gravity z : {grav_z}')
+
+    time.sleep(5)
+
