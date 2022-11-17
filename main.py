5095f1f2575a593625cbff6e202d0bb1a17d16b7
Trent <tdcouse@ncsu.edu>
2022-11-17 16:00:45 -0500

    Updated main

--- main.py
+++ main.py
@@ -7,47 +7,52 @@ sys.path.append("./aprs_decoding/test")
 
 from APRSInterface import APRSInterface
 
+# these are the GPIO pins we are controlling the relay switch with
+ANTENNA_1_PIN = 17
+ANTENNA_2_PIN = 27
+
 def main():
-    aprsInterface = APRSInterface()
+    aprs_interface = APRSInterface()
 
-    chooseAntenna()
+    choose_antenna()
 
-    # aprsInterface.startRecv()
+    aprs_interface.startRecv()
 
     try:
         while True:
-            chooseAntenna()
+            # chooseAntenna()
+            pass
     except KeyboardInterrupt:
         pass
 
-    # aprsInterface.stop()
+    aprs_interface.stop()
 
-def chooseAntenna():
+def choose_antenna():
     sensor = BNOInterface.BNOInterface()
-    antenna_1_pin = 17
-    antenna_2_pin = 27
 
     # setup board
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
 
     # set output pins to output
-    GPIO.setup(antenna_1_pin, GPIO.OUT)
-    GPIO.setup(antenna_2_pin, GPIO.OUT)
+    GPIO.setup(ANTENNA_1_PIN, GPIO.OUT)
+    GPIO.setup(ANTENNA_2_PIN, GPIO.OUT)
 
     #setup orientation determination
     angle = 0
+    # TODO: Make sure we know how the IMU is oriented before calling z
+    print('IMU ORIENTATION HAS NOT BEEN CONFIRMED')
     angle = sensor.get_euler()[2] #using the z-axis euler angle to determine orientation
     print(angle)
 
     if angle in range(0, 135) or angle in range (315, 360):
-        GPIO.output(antenna_2_pin, True)
-        GPIO.output(antenna_1_pin, False)
+        GPIO.output(ANTENNA_2_PIN, True)
+        GPIO.output(ANTENNA_1_PIN, False)
 
         print("Chose antenna 2")
     elif angle in range(135, 315):
-        GPIO.output(antenna_1_pin, True)
-        GPIO.output(antenna_2_pin, False)
+        GPIO.output(ANTENNA_1_PIN, True)
+        GPIO.output(ANTENNA_2_PIN, False)
 
         print("Chose antenna 1")
 
