205112f627fc6e889127192c33815832a2e8410e
Trent <tdcouse@ncsu.edu>
2022-11-18 17:44:00 +0000

    Updated BNO interface to only return useful data

--- BNOInterface.py
+++ BNOInterface.py
@@ -1,6 +1,7 @@
 import board
 import busio
 import adafruit_bno055
+import time
 
 
 class BNOInterface:
@@ -18,6 +19,10 @@ class BNOInterface:
         This is a 3-tuple of X, Y, Z axis accelerometer values in meters per
         second squared.
         """
+        while True:
+            # only break once any of the values are not 0
+            if not all(val == 0 for val in self.sensor.acceleration):
+                break
         return self.sensor.acceleration
     
     def get_linear_acceleration(self) -> tuple:
@@ -25,6 +30,10 @@ class BNOInterface:
         This is a 3-tuple of X, Y, Z linear acceleration values (i.e. without
         effect of gravity) in meters per second squared.
         """
+        while True:
+            # only break once any of the values are not 0
+            if not all(val == 0 for val in self.sensor.linear_acceleration):
+                break
         return self.sensor.linear_acceleration
 
     def get_gravity(self) -> tuple:
@@ -32,24 +41,46 @@ class BNOInterface:
         This is a 3-tuple of X, Y, Z gravity acceleration values (i.e. without
         the effect of linear acceleration) in meters per second squared.
         """
+        # this is the initialized value of gravity for the IMU
+        initial_gravity = 9.8
+        # counter in case IMU is perfectly level
+        counter = 0
+        while counter < 100:
+            # only break once any of the values are not 0
+            if not all(val == 0.0 for val in self.sensor.gravity):
+                if self.sensor.gravity[2] != initial_gravity:
+                    break
+            counter += 1
         return self.sensor.gravity
     
     def get_euler(self) ->  tuple:
         """
         This is a 3-tuple of orientation Euler angle values.
         """
+        while True:
+            # only break once any of the values are not 0
+            if not all(angle == 0.0 for angle in self.sensor.euler):
+                break
         return self.sensor.euler
 
     def get_quaternion(self) -> tuple:
         """
         This is a 4-tuple of orientation quaternion values.
         """
+        while True:
+            # only break once any of the values are not 0
+            if not all(angle == 0.0 for angle in self.sensor.quaternion):
+                break
         return self.sensor.quaternion
 
     def get_magnetic(self) -> tuple:
         """
         This is a 3-tuple of X, Y, Z axis magnetometer values in micro-teslas.
         """
+        while True:
+            # only break once any of the values are not 0.0
+            if not all(mag == 0 for mag in self.sensor.magnetic):
+                break
         return self.sensor.magnetic
 
     def get_gyro(self) -> tuple:
@@ -57,10 +88,16 @@ class BNOInterface:
         This is a 3-tuple of X, Y, Z axis gyroscope values in degrees per
         second.
         """
+        while True:
+            # only break once any of the values are not 0
+            if not all(val == 0.0 for val in self.sensor.gyro):
+                break
         return self.sensor.gyro
 
     def get_temperature(self) -> float:
         """
-        The sensor temperature in degrees Celsius.
+        The sensor temperature in degrees Celsius. The
         """
+        while self.sensor.temperature < 0:
+            continue
         return self.sensor.temperature
--- bno_test.py
+++ bno_test.py
@@ -1,46 +1,16 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
+from BNOInterface import BNOInterface
 import time
-import board
-import busio
-import adafruit_bno055
 
 
-i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_bno055.BNO055_I2C(i2c)
-
-# If you are going to use UART uncomment these lines
-# uart = board.UART()
-# sensor = adafruit_bno055.BNO055_UART(uart)
-
-last_val = 0xFFFF
-
-
-def temperature():
-    global last_val  # pylint: disable=global-statement
-    result = sensor.temperature
-    if abs(result - last_val) == 128:
-        result = sensor.temperature
-        if abs(result - last_val) == 128:
-            return 0b00111111 & result
-    last_val = result
-    return result
-
+sensor = BNOInterface()
 
 while True:
-    print("Temperature: {} degrees C".format(sensor.temperature))
-    """
-    print(
-        "Temperature: {} degrees C".format(temperature())
-    )  # Uncomment if using a Raspberry Pi
-    """
-    print("Accelerometer (m/s^2): {}".format(sensor.acceleration))
-    print("Magnetometer (microteslas): {}".format(sensor.magnetic))
-    print("Gyroscope (rad/sec): {}".format(sensor.gyro))
-    print("Euler angle: {}".format(sensor.euler))
-    print("Quaternion: {}".format(sensor.quaternion))
-    print("Linear acceleration (m/s^2): {}".format(sensor.linear_acceleration))
-    print("Gravity (m/s^2): {}".format(sensor.gravity))
+    print(f"Accelerometer (m/s^2): {sensor.get_acceleration()}")
+    print(f"Magnetometer (microteslas): {sensor.get_magnetic()}")
+    print(f"Gyroscope (rad/sec): {sensor.get_gyro()}")
+    print(f"Euler angle: {sensor.get_euler()}")
+    print(f"Quaternion: {sensor.get_quaternion()}")
+    print(f"Linear acceleration (m/s^2): {sensor.get_linear_acceleration()}")
+    print(f"Gravity (m/s^2): {sensor.get_gravity()}")
     print()
     time.sleep(1)
