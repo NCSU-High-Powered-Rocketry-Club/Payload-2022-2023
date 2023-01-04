8ef9ed48b4ca4d96b5c406e005f09d07f6931185
fcmcbrid <fcmcbrid@ncsu.edu>
2023-01-04 15:34:41 -0500

    Removed an image, added an image, added test file

--- test_meme.py
+++ test_meme.py
@@ -1,30 +1,11 @@
 from PIL import Image
 from PIL import ImageDraw
-import deeppyer
+import deeppyer, asyncio
 
-#def applyfilter(): # This calls imageFilter module
-#    import imageFilter
-#    image = Image.open('pic.jpg')
-#    image_data = image.load()
-#    height,width = image.size
-#    imageFilter.meme(image, height, width) # This is a placeholder, replace w random number generator
-#    image.show()
+async def fry():
+    img = Image.open('pic.jpg')
+    img = await deeppyer.deepfry(img)
+    img.save('fried.jpg')
 
-#def applyfilter(): # This calls imageFilter module
-#    import imageFilter
-#    image = Image.open('pic.jpg')
-#    image_data = image.load()
-#    height,width = image.size
-#    imageFilter.grassless(image, image_data, height, width) # This is a placeholder, replace w random number generator
-#    image.show()
-#applyfilter()
-
-image = Image.open('pic.jpg')
-image_data = image.load()
-
-def fry(image):
-    image = deeppyer.deepfry(image)
-    image.show()
-    image.save('fried.jpg')
-
-fry(image)
+loop = asyncio.get_event_loop()
+loop.run_until_complete(fry())
