d6ce232f1e3a886420639dee518103154edcbcc1
fcmcbrid <fcmcbrid@ncsu.edu>
2023-01-31 18:34:31 -0500

    Changed testservo to be universal

--- testServo.py
+++ testServo.py
@@ -1,11 +1,16 @@
+import RPi.GPIO as GPIO
 from Servo import Servo
 import time
 
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(18,GPIO.OUT)
+pwm = GPIO.PWM(18, 1000)
+pwm.start(50)
+
 def testServo():
-    myServo = Servo(13, 1, 50) # Define class Servo in this file
-    myServo.set_degrees(0)
+    pwm.ChangeDutyCycle(0)
     time.sleep(3)
-    myServo.set_degrees(180)
+    pwm.ChangeDutyCycle(100)
     time.sleep(3)
 
 while True:
