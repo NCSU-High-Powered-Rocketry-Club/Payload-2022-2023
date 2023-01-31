8258abf14e6aa128077926f705427de3821d2b21
fcmcbrid <fcmcbrid@ncsu.edu>
2023-01-31 18:27:17 -0500

    Changed duty cycle on testServo

--- testServo.py
+++ testServo.py
@@ -2,7 +2,7 @@ from Servo import Servo
 import time
 
 def testServo():
-    myServo = Servo(13, 0, 100) # Define class Servo in this file
+    myServo = Servo(13, 1, 1000) # Define class Servo in this file
     myServo.set_degrees(0)
     time.sleep(3)
     myServo.set_degrees(180)
