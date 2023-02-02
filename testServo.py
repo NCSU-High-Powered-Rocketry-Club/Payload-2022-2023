8fae4d5400b07c18d20b2938ef2a7cea00d957b9
fcmcbrid <fcmcbrid@ncsu.edu>
2023-02-02 18:52:52 -0500

    Changed some things

--- testServo.py
+++ testServo.py
@@ -2,15 +2,18 @@ import RPi.GPIO as GPIO
 from Servo import Servo
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18,GPIO.OUT)
-pwm = GPIO.PWM(18, 490)
-pwm.start(50)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(19,GPIO.OUT)
+pwm = GPIO.PWM(19, 50)
+pwm.start(0)
 
 def testServo():
     print('0')
-    pwm.ChangeDutyCycle(100)
-    print('100')
+    pwm.ChangeDutyCycle(5)
+    print('5')
+    pwm.ChangeDutyCycle(7.5)
+    print(7.5)
+    pwm.ChangeDutyCycle(10)
 
 while True:
     testServo()
