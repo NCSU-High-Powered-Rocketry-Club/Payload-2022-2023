7d3ba85a794d29cfed30fc40db9c00d783c457fe
fcmcbrid <fcmcbrid@ncsu.edu>
2023-02-21 19:51:09 -0500

    Testing

--- moveServo.py
+++ moveServo.py
@@ -51,7 +51,7 @@ def turn_to_second(turns):
 def better_sleep(ns):
     start = time.time_ns()
     while True:
-        if (time.time_ns() - start) > 2:
+        if (time.time_ns() - start) > 4:
             break
 
 if __name__ == "__main__":
