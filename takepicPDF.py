944080518711d83faa8faf7f4624b5b33e33135d
fcmcbrid <fcmcbrid@ncsu.edu>
2023-02-24 16:57:37 -0500

    Updated PDF takepic

--- takepicPDF.py
+++ takepicPDF.py
@@ -1,16 +1,6 @@
-import RPi.GPIO as GPIO
-import libcamera
+import RPi.GPIO as gp
 import os
-import numpy as np
-from PIL import Image
-import time
-import cv2
 
 def takepicPDF(cam, x):
-    camera = libcamera.Camera()
-    camera.start()
-    buffer = camera.capture()
-    image_data = np.frombuffer(buffer, dtype=np.uint8)
-    image_
-    time.sleep(2)
-    image = rawCapture.array
+    cmd = "libcamera-still -n -o capture_%s_%d.jpg" % (cam, x)
+    os.system(cmd)
