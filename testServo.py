ba2f7639b2cc1c8641e1be45bf8ee02e3a1f9d9d
Trent <tdcouse@ncsu.edu>
2023-03-21 20:51:44 +0000

    let's see

--- testServo.py
+++ testServo.py
@@ -1,25 +1,30 @@
 import RPi.GPIO as GPIO
 import re
 import moveServo
+import time
 
 GPIO.setmode(GPIO.BOARD)
 
 pinServo = 24
 GPIO.setup(pinServo,GPIO.OUT)
 pwm = GPIO.PWM(pinServo, 50)
-moveServo(60, pwm)
+moveServo.moveServo(60, pwm)
+time.sleep(2)
 
 pinServo = 21
 GPIO.setup(pinServo,GPIO.OUT)
 pwm = GPIO.PWM(pinServo, 50)
-moveServo(60, pwm)
+moveServo.moveServo(60, pwm)
+time.sleep(2)
 
 pinServo = 23
 GPIO.setup(pinServo,GPIO.OUT)
 pwm = GPIO.PWM(pinServo, 50)
-moveServo(60, pwm)
+moveServo.moveServo(60, pwm)
+time.sleep(2)
 
 pinServo = 22
 GPIO.setup(pinServo,GPIO.OUT)
 pwm = GPIO.PWM(pinServo, 50)
-moveServo(60, pwm)
+moveServo.moveServo(60, pwm)
+time.sleep(2)
