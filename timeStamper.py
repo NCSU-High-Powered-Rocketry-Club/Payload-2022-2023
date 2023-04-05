7db3c9ea5af7d464c20ff792ab0fa54382a3fa8e
fcmcbrid <fcmcbrid@ncsu.edu>
2023-04-05 17:32:14 -0400

    whoops now it's finished

--- timeStamper.py
+++ timeStamper.py
@@ -1,16 +1,11 @@
 from PIL import Image, ImageDraw, ImageFont
 from datetime import datetime
 
-def timeStamper(currentTime):
+def timeStamper(currentTime, picName):
     timeStampMsg = currentTime.strftime("%Y.%m.%d - %H:%H:%S")
-    img = Image.open("dirtytest.png")
+    img = Image.open(picName)
     I1 = ImageDraw.Draw(img)
     [l,w] = img.size
     font = ImageFont.truetype("impact.ttf", size=48)
     I1.text((0.025*l, 0.9*w), timeStampMsg, fill=(255,0,0),font=font)
-    img.save("dirtytest2.png")
-
-
-
-currentTime = datetime.now()
-timeStamper(currentTime)
+    img.save(picName)
