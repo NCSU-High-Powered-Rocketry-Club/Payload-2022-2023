ceb47d1a96dc2d7c68e63b5fecfcaf0b0d0fbca3
Frances <fcmcbrid@ncsu.edu>
2023-04-05 22:29:26 +0100

    Finished timeStamper.py demo

--- timeStamper.py
+++ timeStamper.py
@@ -6,9 +6,9 @@ def timeStamper(currentTime):
     img = Image.open("dirtytest.png")
     I1 = ImageDraw.Draw(img)
     [l,w] = img.size
-    font = ImageFont.truetype("impact.ttf", size=12)
+    font = ImageFont.truetype("impact.ttf", size=48)
     I1.text((0.025*l, 0.9*w), timeStampMsg, fill=(255,0,0),font=font)
-    img.save(picName)
+    img.save("dirtytest2.png")
 
 
 
